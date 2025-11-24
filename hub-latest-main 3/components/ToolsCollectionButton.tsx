ecd3f752f525b9627be30eaa530e6bbdd6cbe4b2
haha321-haha <dutinglove@gmail.com>
2025-11-24 09:15:42 +0800

    chore: 删除 hub-latest-main 3 目录

--- hub-latest-main 3/components/ToolsCollectionButton.tsx
+++
@@ -1,2 +0,0 @@
-// This component has been removed as downloads page is no longer needed
-// All PDF downloads are now handled through the articles page
--- hub-latest-main 3/i18n/index.ts
+++
@@ -1,3 +0,0 @@
-// 统一的i18n导出文件
-export { locales, defaultLocale, type Locale } from './request';
-export const localePrefix = 'always' as const;
